87de50994284ba325b31bdc545f40e5afca39375
fantasticliar <18883287680@163.com>
2019-11-09 00:26:57 +0800

    git init project

---
+++ src/main/java/com/imlegend/garbage_classfication/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.imlegend.garbage_classfication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
